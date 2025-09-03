4fb1e65b82611db8f8d99ed43cf8f07732a3218d
beltus78 <beltus78@gmail.com>
2025-09-03 15:16:04 +0500

    Add SMS API key to Netlify function environment

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 // Auto-generated version info
 export const VERSION_INFO = {
   "version": "1.0.1",
-  "gitCommit": "5cbcb1b27dc258ad542d28194ef20a385ecc0630",
-  "buildDate": "2025-09-03T09:23:02.394Z",
+  "gitCommit": "ce215a055965b9126b91438fc7c215e019e6040c",
+  "buildDate": "2025-09-03T09:28:14.021Z",
   "environment": "production"
 };
